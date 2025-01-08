9062b43fa5846a8eefa1ad7700e5f978c2adca4a
psikoo <hugohernandezmuelas03@gmail.com>
2025-01-08 09:50:22 +0100

    made the base url look better

--- aboutme-backend/src/app.service.ts
+++ aboutme-backend/src/app.service.ts
@@ -1,20 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { app } from './main';
+import { Router } from 'express';
 
 @Injectable()
 export class AppService {
   getRouts(): JSON {
     const server = app.getHttpServer();
-    const router = server._events.request._router;
-    let availableRoutes: [{ path: string, method: string }?] = [];
+    const router: Router = server._events.request._router;
+    let availableRoutes: [{ path: string, methods: string[] }?] = [];
     for(let i=0; i<router.stack.length; i++) {
-      if (router.stack[i].route && router.stack[i].route?.stack[0].method !== "acl") {
+      if (router.stack[i].route && router.stack[i].route?.stack[0].method !== "acl" && isInList(router.stack[i].route?.path, availableRoutes)) {
         availableRoutes.push({
           path: router.stack[i].route?.path,
-          method: router.stack[i].route?.stack[0].method
+          methods: getMethods(router.stack[i].route?.path, router),
         })
       }
     }
     return JSON.parse(JSON.stringify(availableRoutes));
   }
+}
+
+function isInList(path: string, availableRoutes: [{ path: string, methods: string[] }?]): boolean {
+  if(path.endsWith('/:id')) return false;
+  for(let i=0; i<availableRoutes.length; i++) {
+    if(availableRoutes[i].path === path) return false;
+  }
+  return true;
+}
+
+function getMethods(path: string, router: Router): string[] {
+  let methods: string[] = [];
+  console.log(path)
+  for(let i=0; i<router.stack.length; i++) {
+    if(router.stack[i].route?.path === path) {
+      methods.push(router.stack[i].route?.stack[0].method)
+    } else if(router.stack[i].route?.path === path+"/:id") {
+      methods.push(router.stack[i].route?.stack[0].method+"/:id")
+    }
+  }
+  return methods;
 }
