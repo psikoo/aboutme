7b14f46d87f18c61328dc4b595c8f94606078eb7
psikoo <hugohernandezmuelas03@gmail.com>
2025-01-13 10:26:02 +0100

    added https to backend

--- aboutme-backend/src/main.ts
+++ aboutme-backend/src/main.ts
@@ -1,8 +1,13 @@
+import fs from 'fs';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 export let app;
+const httpsOptions = {
+  key: fs.readFileSync(process.env.PRIVATE_KEY),
+  cert: fs.readFileSync(process.env.PUBLIC_KEY),
+};
 async function bootstrap() {
   app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
