9643febc9b05f3d0bff8503f4252998078b843a3
psikoo <hugohernandezmuelas03@gmail.com>
2025-01-14 23:21:51 +0100

    foot note

--- aboutme-frontend/y2k/vue/src/components/FooterComponent.vue
+++ aboutme-frontend/y2k/vue/src/components/FooterComponent.vue
@@ -4,13 +4,13 @@
 
 <template>
   <div class="main">
-    <h1>Footer</h1>
+    <h1>This site is under construction x3</h1>
   </div>
 </template>
 
 <style scoped>
   .main {
     width: 100%;
-    background-color: aqua;
+    background-color: red;
   }
 </style>
