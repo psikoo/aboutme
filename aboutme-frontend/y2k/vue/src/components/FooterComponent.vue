f3454120ac31c2679a1f5d06c1c0d1a217f555c0
psikoo <hugohernandezmuelas03@gmail.com>
2025-01-17 02:10:55 +0100

    Update FooterComponent.vue

--- aboutme-frontend/y2k/vue/src/components/FooterComponent.vue
+++ aboutme-frontend/y2k/vue/src/components/FooterComponent.vue
@@ -2,12 +2,14 @@
 import { ref } from "vue";
   const viewW = ref(Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0));
   const viewH = ref(Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0));
+  const userAgent = ref(navigator.userAgent);
 </script>
 
 <template>
   <div class="main">
     <h1>This site is under construction x3</h1>
     <h1>vw: {{ viewW }} | vh: {{ viewH }}</h1>
+    <h1>{{ userAgent }}</h1>
   </div>
 </template>
 
