d5f875bdf59fa3509a6953def9708298bf43fc2a
psikoo <hugohernandezmuelas03@gmail.com>
2025-01-17 16:45:11 +0100

    Update SongComponent.vue

--- aboutme-frontend/y2k/vue/src/components/body/right/SongComponent.vue
+++ aboutme-frontend/y2k/vue/src/components/body/right/SongComponent.vue
@@ -25,13 +25,12 @@
     <div v-if="typeof songs == 'string'">Loading...</div>
     <div v-else>
       <h1 class="tittleTop underline">Music ^///^</h1>
-      <div class="songs">
-        <div v-for="(song, index) in songs" id="vinyl-gallery" class="song">
-          <div class="vinyl">
-            <!-- <a :href="songs[index].url" target="_blank"><img :src="songs[index].cover" alt="Song Cover" class="cover vinyl"></a> -->
-            <img :src="songs[index].cover" alt="Song Cover" class="cover vinyl">
+      <div id="vinyl-gallery">
+        <div v-for="(song, index) in songs" class="vinyl">
+          <img :src="songs[index].cover" alt="Song Cover">
+          <div class="title">
+            <a :href="songs[index].url" target="_blank"><p class="link">{{ songs[index].name }} - {{ songs[index].tag }}</p></a>
           </div>
-          <div class="title">{{ songs[index].name }} - {{ songs[index].tag }} </div>
         </div>
       </div>
     </div>
@@ -53,55 +52,27 @@
     margin-bottom: 10px;
   }
 
-  .songs {
-    width: 560px;
-    margin: auto;
-    display: grid;
-    grid-auto-columns: 1fr;
-    grid-template-columns: auto auto;
-    align-items: start;
-    contain: content;
-  }
-
-  .song  {
-    border: 2px solid var(--border-color);
-    width: 10vw;
-    height: 10vw;
-    text-align: start;
-    padding: 10px;
-    margin: 0px 5px 5px 0px;
-  }
-
-  .cover {
-    height: 10vw;
-  }
-
-  /* Vinyl Showcase - www.kazimariusz.com */
-  #vinyl-gallery * {
-    all: unset;
-  }
-  
   #vinyl-gallery {
     all: initial;
     width: 100%;
     height: 230px;
-    margin: 30px 0;
+    margin-top: 20px;
     display: flex;
     flex-direction: row;
     position: relative;
     margin-left: -6px;
   }
-  
+
   #vinyl-gallery .vinyl {
     perspective: 500px !important;
     width: 18px;
     transition: width 0.5s;
   }
-  
+
   #vinyl-gallery .vinyl:hover {
-    width: 148px;
+    width: 180px;
   }
-  
+
   #vinyl-gallery img {
     transition: transform 0.5s, width 0.5s, height 0.5s, margin-top 0.5s;
     width: 180px;
@@ -111,14 +82,14 @@
     border-radius: 4px;
     border: 2px solid rgba(0, 0, 0, 0.1);
   }
-  
+
   #vinyl-gallery .vinyl:hover img {
     transform: rotateX(0deg) rotateY(10deg);
     width: 188px;
     height: 188px;
     margin-top: -2px;
   }
-  
+
   #vinyl-gallery .title {
     display: block;
     visibility: hidden;
@@ -128,8 +99,17 @@
     width: 100%;
     padding-left: 6px;
   }
-  
-  #vinyl-gallery .vinyl:nth-child(n):hover + .title {
+
+  .title>p, .title>a {
+    text-wrap: nowrap;
+    font-family: "PT Mono", serif;
+    font-weight: 400;
+    font-style: normal;
+    color: var(--text-color);
+    animation: textShadow 1.5s infinite;
+  }
+
+  #vinyl-gallery .vinyl:nth-child(n):hover .title {
     visibility: visible;
   }
 </style>  
