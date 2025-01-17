fa4d462f18721b18935d1ec7edc2b72ce756d9a8
psikoo <hugohernandezmuelas03@gmail.com>
2025-01-17 05:22:16 +0100

    auto redirect test

--- aboutme-frontend/y2k/vue/src/components/FooterComponent.vue
+++ aboutme-frontend/y2k/vue/src/components/FooterComponent.vue
@@ -3,6 +3,9 @@ import { ref } from "vue";
   const viewW = ref(Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0));
   const viewH = ref(Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0));
   const isInstagram = ref(!!navigator.userAgent.match(/Instagram/i));
+  const isAndroid = ref(!!navigator.userAgent.match(/Android/i));
+  const isIOS = ref(!!navigator.userAgent.match(/iPad|iPhone|iPod/i));
+  if(isInstagram && isAndroid) window.location.replace("intent://y2k.cait.moe;scheme=https;end")
   const userAgent = ref(navigator.userAgent);
 </script>
 
@@ -11,6 +14,8 @@ import { ref } from "vue";
     <h1>This site is under construction x3</h1>
     <h1>vw: {{ viewW }}, vh: {{ viewH }} (1:{{ viewW/viewH }})</h1>
     <h1>Instagram: {{ isInstagram }}</h1>
+    <h1>Android: {{ isAndroid }}</h1>
+    <h1>iOS: {{ isIOS }}</h1>
     <h1>userAgent: {{ userAgent }}</h1>
 
     <!--! THIS IS THE BEST TOOL https://inappdebugger.com/ -->
