489377cfde78ff782f127b68716311f4bc16bd56
psikoo <hugohernandezmuelas03@gmail.com>
2025-01-23 04:37:52 +0100

    added basic layout to cv

---
+++ aboutme-frontend/cv/vue/src/components/BodyComponent.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="main">
+    <h1>Body</h1>
+  </div>
+</template>
+
+<style scoped>
+  .main {
+    max-width: 100%;
+    contain: content;
+  }
+</style>
---
+++ aboutme-frontend/cv/vue/src/components/FooterComponent.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="main">
+    <h1>Footer</h1>
+  </div>
+</template>
+
+<style scoped>
+  .main {
+    width: 100%;
+  }
+</style>
