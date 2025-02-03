4f74d8b75426eeca185898da51d0e8cdc48d9360
psikoo <hugohernandezmuelas03@gmail.com>
2025-02-03 13:07:03 +0100

    Update start.sh

--- aboutme-docker/start.sh
+++ aboutme-docker/start.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
+cd ..
+homeRepo=$(pwd)
+varRepo="/var/www/aboutme"
+
 echo "-- Pulling repo --"
-cd .. # home /aboutme
+cd $homeRepo
 sudo git stash 
 sudo git pull
-echo "-- Starting nginx --"
-sudo nginx -t
-sudo nginx -s reload
-echo "-- Starting the backend --"
-cd aboutme-backend # home /aboutme/aboutme-backend
-sudo docker build -t aboutme-backend:1 .
-cd ../aboutme-docker # home /aboutme/aboutme-docker
-sudo docker compose down
-sudo docker compose up -d
-echo "-- Starting the frontend --"
-echo "-- Pulling repo --"
-cd /var/www/aboutme # var /aboutme
+cd $varRepo
 sudo git stash
 sudo git pull
-cd /var/www/aboutme/aboutme-frontend # var /aboutme/aboutme-frontend
-cd y2k/vue
+
+echo "-- Test nginx --"
+sudo nginx -t
+sudo nginx -s reload
+
+echo "-- Build backend --"
+cd $homeRepo/aboutme-backend
+sudo docker build -t aboutme-backend:1 .
+
+echo "-- Building the frontend --"
+cd $varRepo/aboutme-frontend/y2k/vue
 echo "> y2k"
 sudo npm run build
-cd /var/www/aboutme/aboutme-frontend # var /aboutme/aboutme-frontend
-cd cv/vue
+
+cd $varRepo/aboutme-frontend/cv/vue
 echo "> cv"
 sudo npm run build
+
+cd $homeRepo/aboutme-docker
+sudo docker compose down
+sudo docker compose up -d
+
 echo "-- Script End --"
