6a81fa72ca56aac54848c876d66cb69cd2fb1de1
psikoo <hugohernandezmuelas03@gmail.com>
2025-02-03 12:33:22 +0100

    fix

--- aboutme-docker/start.sh
+++ aboutme-docker/start.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 echo "-- Pulling repo --"
-cd ..
+cd .. # home /aboutme
 sudo git stash 
 sudo git pull
+echo "-- Starting nginx --"
+sudo nginx -t
+sudo nginx -s reload
 echo "-- Starting the backend --"
-# home /aboutme/aboutme-backend
-cd ../aboutme-backend
+cd aboutme-backend # home /aboutme/aboutme-backend
 sudo docker build -t aboutme-backend:1 .
-# home /aboutme/aboutme-docker
-cd ../aboutme-docker
+cd ../aboutme-docker # home /aboutme/aboutme-docker
 sudo docker compose down
 sudo docker compose up -d
 echo "-- Starting the frontend --"
-# var /aboutme
 echo "-- Pulling repo --"
-cd /var/www/aboutme
+cd /var/www/aboutme # var /aboutme
 sudo git stash
 sudo git pull
-# var /aboutme/aboutme-frontend
-cd /var/www/aboutme/aboutme-frontend
+cd /var/www/aboutme/aboutme-frontend # var /aboutme/aboutme-frontend
 cd y2k/vue
 echo "> y2k"
 sudo npm run build
-cd /var/www/aboutme/aboutme-frontend
+cd /var/www/aboutme/aboutme-frontend # var /aboutme/aboutme-frontend
 cd cv/vue
 echo "> cv"
 sudo npm run build
