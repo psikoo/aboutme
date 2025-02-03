6f6296f28d01724eb8e7c1ce114dcc16ccb328b5
psikoo <hugohernandezmuelas03@gmail.com>
2025-02-03 10:43:59 +0100

    added a start script

---
+++ aboutme-docker/start.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "-- Pulling repo --"
+cd ..
+sudo git stash 
+sudo git pull
+echo "-- Starting nginx --"
+# home /aboutme/aboutme-nginx
+cd ./aboutme-nginx
+echo "-- Starting the backend --"
+# home /aboutme/aboutme-backend
+cd ../aboutme-backend
+sudo docker build -t aboutme-backend:1 .
+# home /aboutme/aboutme-docker
+cd ../aboutme-docker
+sudo docker compose down
+sudo docker compose up -d
+echo "-- Starting the frontend --"
+# var /aboutme
+echo "-- Pulling repo --"
+cd /var/www/aboutme
+sudo git stash
+sudo git pull
+# var /aboutme/aboutme-frontend
+cd /var/www/aboutme/aboutme-frontend
+cd y2k/vue
+echo "> y2k"
+sudo npm run build
+cd /var/www/aboutme/aboutme-frontend
+cd cv/vue
+echo "> cv"
+sudo npm run build
+echo "-- Script End --"
--- aboutme-docker/startBackend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Starting the backend"
-cd ..
-sudo git pull
-cd aboutme-backend
-sudo docker build -t aboutme-backend .
-cd ../aboutme-docker
-sudo docker compose down
-sudo docker compose up -d
