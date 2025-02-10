0bfff37aa40c74dfb423759df8c9990b173b1cf6
psikoo <hugohernandezmuelas03@gmail.com>
2025-02-10 01:07:22 +0100

    test

--- aboutme-frontend/blog/resources/js/startup.js
+++ aboutme-frontend/blog/resources/js/startup.js
@@ -1,5 +1,10 @@
-document.getElementById("entryButton").onclick = function() {myFunction()};
+import { getURL } from "./get.js";
 
-function myFunction() {
-  document.getElementById("entryButtonPre").innerHTML = "[ ";
+let entryNum = 1;
+let baseURL = "https://blog.cait.moe/entries/";
+let entries = document.getElementById("entries");
+
+for(let i = 0; i < entryNum; i++) {
+  entries.innerHTML += `<div id="${"entry"+i}" class="entry">${i}</div>`;
+  console.log(getURL(baseURL+i+".html"));
 }
