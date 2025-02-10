4d8e8c5a9ba13681bc5ade99b95b0c3b784992a2
psikoo <hugohernandezmuelas03@gmail.com>
2025-02-10 01:09:27 +0100

    Update startup.js

--- aboutme-frontend/blog/resources/js/startup.js
+++ aboutme-frontend/blog/resources/js/startup.js
@@ -5,6 +5,6 @@ let baseURL = "https://blog.cait.moe/entries/";
 let entries = document.getElementById("entries");
 
 for(let i = 0; i < entryNum; i++) {
-  entries.innerHTML += `<div id="${"entry"+i}" class="entry">${i}</div>`;
-  console.log(getURL(baseURL+i+".html"));
+  entries.innerHTML += `<div id="${"entry"+i}" class="entry">${await getURL(baseURL+i+".html")}</div>`;
+  console.log(await getURL(baseURL+i+".html"));
 }
