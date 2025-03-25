165e25b4700de41d87c2ff856a6a912c079854d7
psikoo <hugohernandezmuelas03@gmail.com>
2025-03-25 04:52:43 +0100

    added urls page

---
+++ aboutme-frontend/site/html/url/resources/js/startup.js
@@ -0,0 +1,14 @@
+import { getURL } from "./get.js";
+
+let baseURL = "https://cait.moe:3000/api/shorturl/urls";
+let urls = document.getElementById("urls");
+
+for(let i = entryNum; i > -1; i--) {
+  urls.innerHTML += `${await getURL(baseURL)}`;
+  console.log(await getURL(baseURL));
+}
+
+// for(let i = entryNum; i > -1; i--) {
+//   urls.innerHTML += `<div id="${"entry"+i}" class="entry">${await getURL(baseURL+i+".html")}</div>`;
+//   console.log(await getURL(baseURL+i+".html"));
+// }
