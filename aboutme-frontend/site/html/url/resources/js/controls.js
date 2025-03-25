5f76fbf4388552d3c8f28402055fb4cf27bbf0b5
psikoo <hugohernandezmuelas03@gmail.com>
2025-03-25 06:40:27 +0100

    started ui

--- aboutme-frontend/site/html/url/resources/js/controls.js
+++ aboutme-frontend/site/html/url/resources/js/controls.js
@@ -18,10 +18,19 @@ let send = document.getElementById("send");
 let method = document.getElementById("method");
 let res = document.getElementById("res");
 send.addEventListener("click", async function () {
-  console.log(method.value);
-  if(method.value == "post") res.innerHTML += `${await post()}`;
-  else if(method.value == "patch") res.innerHTML += `${await patch()}`;
-  else if(method.value == "delete") res.innerHTML += `${await del()}`;
+  console.log(`> Sent ${method.value} - ${document.getElementById("api").value} { oldUrl = ${document.getElementById("old").value}, newUrl = ${document.getElementById("new").value}}`);
+  if(method.value == "post") res.innerHTML = processRes(await post());
+  else if(method.value == "patch") res.innerHTML = processRes(await patch());
+  else if(method.value == "delete") res.innerHTML = processRes(await del());
   // Update UI
   urls.innerText = `${await get()}`;
 });
+
+function processRes(res) {
+  res = JSON.parse(res);
+  let string = `<div class="response `;
+  if(res.Error) string += `error">Error: ${res.Error}</div>`;
+  else if(res.error) string += `error">Error: ${res.message}</div>`;
+  else string += `success">Created (${res.id}) - ${res.oldUrl} => ${res.newUrl}</div>`;
+  return string;
+}
