3e0a7f018496b95c91605b6a70803f95d8304960
psikoo <hugohernandezmuelas03@gmail.com>
2025-03-25 13:56:19 +0100

    Update startup.js

--- aboutme-frontend/site/html/url/resources/js/startup.js
+++ aboutme-frontend/site/html/url/resources/js/startup.js
@@ -2,13 +2,15 @@ import { get, getAll } from "./http.js";
 import { formatUrls } from "./format.js";
 
 let params = new URLSearchParams(document.location.search);
+
 let redirectById = params.get("i");
-let redirectByName = params.get("u");
 if(redirectById != null) {
   let url = JSON.parse(await get(redirectById)).oldUrl;
   console.log(`Redirect by id - (${redirectById}) to ${url}`);
   window.location.href = url;
 } 
+
+let redirectByName = params.get("u");
 if(redirectByName != null) {
   let urls = JSON.parse(await getAll());
   for(let i = 0; i < urls.length; i++) {
