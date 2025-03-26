e7c926ceed6970f28a78c77337ef3ff2c67155aa
psikoo <hugohernandezmuelas03@gmail.com>
2025-03-26 03:17:14 +0100

    optimazied js preloading

--- aboutme-frontend/site/html/url/resources/js/controls.js
+++ aboutme-frontend/site/html/url/resources/js/controls.js
@@ -1,23 +1,26 @@
-import { post } from "./http.js";
-import { processRes, formatUrls } from "./format.js";
+import { post } from "./util/http.js";
+import { processRes } from "./util/format.js";
 
-let show = document.getElementById("show");
-let api = document.getElementById("api");
-let passwordCounter = 0;
-show.addEventListener("click", function () {
-  if(passwordCounter%2 == 0) {
-    api.type="text";
-    show.innerHTML = "<img src='./resources/svg/hide.svg' alt='hide'>";
-  } else {
-    api.type="password";
-    show.innerHTML = "<img src='./resources/svg/show.svg' alt='show'>";
-  }
-  passwordCounter++;
-});
-
-let resDiv = document.getElementById("res");
-let postDiv = document.getElementById("post");
-postDiv.addEventListener("click", async function () {
-  console.log(`> Sent POST - {oldUrl = ${document.getElementById("old").value} > ${document.getElementById("new").value}}`);
-  resDiv.innerHTML = processRes(await post(), "POST");
-});
+let params = new URLSearchParams(document.location.search);
+if(params.get("i") == null && params.get("u") == null ) {
+  let show = document.getElementById("show");
+  let api = document.getElementById("api");
+  let passwordCounter = 0;
+  show.addEventListener("click", function () {
+    if(passwordCounter%2 == 0) {
+      api.type="text";
+      show.innerHTML = "<img src='./resources/svg/hide.svg' alt='hide'>";
+    } else {
+      api.type="password";
+      show.innerHTML = "<img src='./resources/svg/show.svg' alt='show'>";
+    }
+    passwordCounter++;
+  });
+  
+  let resDiv = document.getElementById("res");
+  let postDiv = document.getElementById("post");
+  postDiv.addEventListener("click", async function () {
+    console.log(`> Sent POST - {oldUrl = ${document.getElementById("old").value} > ${document.getElementById("new").value}}`);
+    resDiv.innerHTML = processRes(await post(), "POST");
+  });
+}
--- aboutme-frontend/site/html/url/resources/js/startup.js
+++ aboutme-frontend/site/html/url/resources/js/startup.js
@@ -1,32 +1,9 @@
-import { get, getAll } from "./http.js";
-import { formatUrls } from "./format.js";
+import { formatUrls } from "./util/format.js";
 
-//* Early redirects
 let params = new URLSearchParams(document.location.search);
-//  By id (faster)
-let redirectById = params.get("i");
-if(redirectById != null) {
-  document.body.innerHTML = "";
-  let url = JSON.parse(await get(redirectById)).oldUrl;
-  console.log(`Redirect by id - (${redirectById}) to ${url}`);
-  window.location.href = url;
-} 
-//  By name (readable)
-let redirectByName = params.get("u");
-if(redirectByName != null) {
-  document.body.innerHTML = "";
-  let urls = JSON.parse(await getAll());
-  for(let i = 0; i < urls.length; i++) {
-    if(urls[i].newUrl == "https://url.cait.moe/?u="+redirectByName) {
-      window.location.href = urls[i].oldUrl;
-    }
-  }
-}
-//* Early redirects
-
-// Initial format
-formatUrls();
-
-var refreshUI = window.setInterval(function(){
+if(params.get("i") == null && params.get("u") == null ) {
   formatUrls();
-}, 500);
+  var refreshUI = window.setInterval(function(){
+    formatUrls();
+  }, 500);
+}
