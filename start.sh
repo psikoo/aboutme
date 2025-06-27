178e9f2a2dcf1f78045d12ddd624d004cf1818fb
psikoo <91342186+psikoo@users.noreply.github.com>
2025-06-27 20:46:18 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -40,6 +40,8 @@ sudo npm run build >/dev/null
 cd $varRepo/frontend-caitmoe/site/html/blog
 echo "> blog"
 fileNum=$(sudo ls ./entries | wc -l)
+sudo touch ./entries/counter.txt
+sudo chmod +666 ./entries/counter.txt
 sudo echo $fileNum > ./entries/counter.txt
 
 cd $homeRepo/docker-caitmoe
