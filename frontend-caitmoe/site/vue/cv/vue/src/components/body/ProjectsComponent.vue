009e0b42823f4a4d26f5dbac53ab2ddcf7222b4c
psikoo <91342186+psikoo@users.noreply.github.com>
2025-06-27 20:49:21 +0200

    caitmoe

--- frontend-caitmoe/site/vue/cv/vue/src/components/body/ProjectsComponent.vue
+++ frontend-caitmoe/site/vue/cv/vue/src/components/body/ProjectsComponent.vue
@@ -10,7 +10,7 @@
     <p><a href="https://github.com/psikoo/EAN-13_generator" target="_blank"><strong>EAN-13 generator</strong></a> - <img alt="filetype" class="filetype" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"/>  Generate barcodes using the EAN-13 guidelines</p>
     <p><a href="https://github.com/psikoo/lichessEncrypter" target="_blank"><strong>Lichess Encrypter</strong></a> - <img alt="filetype" class="filetype" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"/>  Encrypts messages as lichess.org games</p>
     <p><a href="https://github.com/psikoo/AlgorithmicArtistry" target="_blank"><strong>Algorithmic Artistry</strong></a> - <img alt="filetype" class="filetype" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg"/> Text based RPG, with turned based combat</p>
-    <p><a href="https://github.com/psikoo/aboutme" target="_blank"><strong>About Me</strong></a> - <img alt="filetype" class="filetype" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"/> Monorepo containing all the code for my sites</p>
+    <p><a href="https://github.com/psikoo/caitmoe" target="_blank"><strong>About Me</strong></a> - <img alt="filetype" class="filetype" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"/> Monorepo containing all the code for my sites</p>
     <p><a href="https://github.com/psikoo?tab=repositories" target="_blank"><strong>More Projects</strong></a> - More personal projects in my github</p>
   </div>
 </template>
@@ -20,7 +20,7 @@
     max-width: 100%;
     contain: content;
   }
-  
+
   hr {
     width: 5%;
     margin: 0px 0px 5px 0px;
